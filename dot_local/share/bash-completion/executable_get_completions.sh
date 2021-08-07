50a02a2ea77e8dcc5e13b5dc18cfe2d525ecb6b3
Vadim-Valdis Yudaev <judaew@outlook.de>
2021-08-07 16:27:03 +0300

    bash: Update get_completions and make java-set to simplify

--- dot_local/share/bash-completion/executable_get_completions.sh
+++ dot_local/share/bash-completion/executable_get_completions.sh
@@ -6,16 +6,3 @@ TMUX_URL="https://raw.githubusercontent.com/imomaliev/tmux-bash-completion/maste
 wget $TMUX_URL -O $COMPLETION_PATH/tmux.bash && \
     echo "tmux.bash downloaded" || \
     echo "tmux.bash failed"
-
-TMUXINATOR_URL="https://raw.githubusercontent.com/tmuxinator/tmuxinator/master/completion/tmuxinator.bash"
-wget $TMUXINATOR_URL -O $COMPLETION_PATH/tmuxinator.bash && \
-    echo "tmuxinator.bash downloaded" || \
-    echo "tmuxinator.bash failed"
-
-chezmoi completion bash --output $COMPLETION_PATH/chezmoi.bash && \
-    echo "chezmoi.bash downloaded" || \
-    echo "chezmoi.bash failed"
-
-gh completion -s bash > $COMPLETION_PATH/gh.bash && \
-    echo "gh.bash download" || \
-    echo "gh.bash failed"
