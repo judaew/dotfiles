7558a860f25e108d907c9595a4c4b2a58f71c652
Vadim-Valdis Yudaev <judaew@outlook.de>
2021-10-17 01:08:12 +0300

    nvim: Add LSP config for lua-language-server

--- .config/nvim/lua/plugin/lsp/lspconfig.lua
+++ .config/nvim/lua/plugin/lsp/lspconfig.lua
@@ -5,3 +5,26 @@ lspconfig.clangd.setup {
     capabilities = cmp_nvim_lsp.update_capabilities(vim.lsp.protocol.make_client_capabilities());
     cmd = { "clangd-mp-12", "--background-index" };
 }
+
+lspconfig.sumneko_lua.setup {
+    cmd = {"/opt/local/bin/lua-language-server"};
+    settings = {
+        Lua = {
+            runtime = {
+                version = 'LuaJIT',
+                path = vim.split(package.path, ';')
+            },
+            diagnostics = {
+                -- Get the language server to recognize the `vim` global
+                globals = {'vim'},
+            },
+            workspace = {
+                -- Make the server aware of Neovim runtime files
+                library = {[vim.fn.expand('$VIMRUNTIME/lua')] = true}
+            },
+            telemetry = {
+                enable = false,
+            }
+        }
+    }
+}
