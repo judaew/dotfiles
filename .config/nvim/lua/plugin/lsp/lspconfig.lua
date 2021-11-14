a6a1e9c2750d89da3b4aa0228494eb2c0ec3d6a4
Vadim-Valdis Yudaev <judaew@outlook.de>
2021-11-14 14:48:18 +0200

    nvim: Perf LSP for the clangd

--- .config/nvim/lua/plugin/lsp/lspconfig.lua
+++ .config/nvim/lua/plugin/lsp/lspconfig.lua
@@ -3,11 +3,12 @@ local cmp_nvim_lsp = require'cmp_nvim_lsp'
 
 lspconfig.clangd.setup {
     capabilities = cmp_nvim_lsp.update_capabilities(vim.lsp.protocol.make_client_capabilities());
-    cmd = { "clangd-mp-12", "--background-index" };
+    cmd = { 'clangd-mp-12', '--background-index', '--suggest-missing-includes',
+            '--clang-tidy', '--header-insertion=iwyu' };
 }
 
 lspconfig.sumneko_lua.setup {
-    cmd = {"/opt/local/bin/lua-language-server"};
+    cmd = {'/opt/local/bin/lua-language-server'};
     settings = {
         Lua = {
             runtime = {
