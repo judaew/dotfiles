bcdc7128a109dac2b33c38ff9ef0878a9d9bcc76
Vadim-Valdis Yudaev <judaew@outlook.de>
2022-02-07 00:42:07 +0200

    nvim: Delete include a non-existent file

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -10,6 +10,3 @@ vim.api.nvim_command 'runtime mappings.vim'
 -- Plugins and their setup
 require 'plugin/packer'
 require 'init/packer_compiled'
-
--- Functions
-vim.api.nvim_command 'runtime viml/func/last_jump.vim'
