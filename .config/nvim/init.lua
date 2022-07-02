f12f1fda6d7d75554b1ee19306f3d3b38f49a1be
Vadym-Valdis Yudaiev <judaew@outlook.de>
2022-07-02 22:41:45 +0300

    Revert "nvim: Temporarily use VimL-like config for init file"
    This reverts commit 2b28f2b924ae20f3fb65cb3c1099c02380855034.

---
+++ .config/nvim/init.lua
@@ -0,0 +1,12 @@
+-- ######################
+-- ### NVIM INIT FILE ###
+-- ######################
+
+-- Basic settings
+require 'options'
+require 'autocommands'
+require 'keymaps'
+
+-- Plugins and their setup
+require 'plugins'
+require 'init/packer_compiled'
