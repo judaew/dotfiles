bcf056a31d1915597d33028882452ce186cf430e
Vadym-Valdis Yudaiev <judaew@outlook.de>
2023-03-04 15:46:59 +0200

    nvim: Refactor lsp config and keymaps
    Inspired by https://github.com/nvim-lua/kickstart.nvim/blob/4a37a0a9b1d53fd355637256ac975c619226fb6d/init.lua

--- .config/nvim/lua/plugins/ide/lsp.lua
+++ .config/nvim/lua/plugins/ide/lsp.lua
@@ -1,10 +1,44 @@
-local lspconfig = require'lspconfig'
--- See https://github.com/hrsh7th/cmp-nvim-lsp/tree/59224771f91b86d1de12570b4070fe4ad7cd1eeb#capabilities
-local compose_capabilities = require('cmp_nvim_lsp').default_capabilities()
+local on_attach = function(_, bufnr)
+    local keymap = function(keys, func, desc)
+        if desc then
+            desc = 'LSP: ' .. desc
+        end
 
-lspconfig.clangd.setup {
-    capabilities = compose_capabilities,
-    cmd = { vim.fn.exepath('clangd-mp-15'),
+        vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+    end
+
+    keymap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+    -- keymap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+    keymap('<leader>ca', require('code_action_menu').open_code_action_menu, '[C]ode [A]ction')
+
+    keymap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+    keymap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+    keymap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
+    keymap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
+    keymap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+    keymap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+
+    -- See `:help K` for why this keymap
+    keymap('K', vim.lsp.buf.hover, 'Hover Documentation')
+    keymap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+
+    -- Lesser used LSP functionality
+    keymap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+    keymap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
+    keymap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
+    keymap('<leader>wl', function()
+        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, '[W]orkspace [L]ist Folders')
+
+    -- Create a command `:Format` local to the LSP buffer
+    vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
+        vim.lsp.buf.format()
+    end, { desc = 'Format current buffer with LSP' })
+end
+
+local servers = {
+    clangd = {
+        cmd = { vim.fn.exepath('clangd-mp-15'),
             '--all-scopes-completion',
             '--suggest-missing-includes',
             '--background-index',
@@ -13,7 +47,7 @@ lspconfig.clangd.setup {
             '--completion-style=detailed',
             '--clang-tidy',
             '--clang-tidy-checks=-*,llvm-*,clang-analyzer-*,modernize-*,-modernize-use-trailing-return-type',
-            '--fallback-style=LLVM',
+            '--fallback-style=WebKit',
             '--header-insertion=never',
 
             -- clangd 11+ supports reading from .clangd configuration file
@@ -21,35 +55,43 @@ lspconfig.clangd.setup {
 
             -- store PCHs in RAM
             '--pch-storage=memory'}
-}
-
-lspconfig.gopls.setup {}
-
-lspconfig.lua_ls.setup {
-    capabilities = compose_capabilities,
-    cmd = {vim.fn.exepath('lua-language-server')};
-    settings = {
-        Lua = {
-            runtime = {
-                version = 'LuaJIT',
-                path = vim.split(package.path, ';')
-            },
-            diagnostics = {
-                -- Get the language server to recognize the `vim` global
-                globals = {'vim', 'use'},
-            },
-            workspace = {
-                -- Make the server aware of Neovim runtime files
-                library = {[vim.fn.expand('$VIMRUNTIME/lua')] = true}
-            },
-            telemetry = {
-                enable = false,
+    },
+    gopls = {},
+    lua_ls = {
+        cmd = {vim.fn.exepath('lua-language-server')};
+        settings = {
+            Lua = {
+                runtime = {
+                    version = 'LuaJIT',
+                    path = vim.split(package.path, ';')
+                },
+                diagnostics = {
+                    -- Get the language server to recognize the `vim` global
+                    -- globals = {'vim', 'use'},
+                },
+                workspace = { checkThirdParty = false },
+                telemetry = {
+                    enable = false,
+                }
             }
         }
+    },
+    pyright = {
+        cmd = { vim.fn.exepath('pyright-langserver'), "--pythonversion 3.11", "--stdio", }
     }
 }
 
-lspconfig.pyright.setup {
-    capabilities = compose_capabilities,
-    cmd = {vim.fn.exepath('pyright-langserver'), "--pythonversion 3.11", "--stdio",}
-}
+-- nvim-cmp supports additional completion capabilities, so broadcast that 
+-- to servers.See https://github.com/hrsh7th/cmp-nvim-lsp/tree/59224771f91b86d1de12570b4070fe4ad7cd1eeb#capabilities
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+
+require 'neodev'.setup()
+
+for i in pairs(servers) do
+    require 'lspconfig'[i].setup {
+        capabilities = capabilities,
+        on_attach = on_attach,
+        settings = servers[i]
+    }
+end
