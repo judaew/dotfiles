39bf221ba6217f3ec649653baa5248034e151f90
Vadym-Valdis Yudaiev <judaew@outlook.de>
2023-03-04 16:47:36 +0200

    nvim: Add diagnostic keymaps

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -62,4 +62,8 @@ keymap("", "<Leader>P", "\"P", opts)
 -- Toggle for wrap options
 keymap("n", "<Leader>w", ":set wrap!<CR>", opts)
 
-
+-- Diagnostic keymaps
+keymap('n', '[d', vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
+keymap('n', ']d', vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
+keymap('n', '<leader>e', vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
+keymap('n', '<leader>q', vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
