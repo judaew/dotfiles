9ad5235a96ca91554ded0fe47317c6f9a9697e08
Vadym-Valdis Yudaiev <judaew@outlook.de>
2023-07-29 01:45:03 +0300

    nvim(alpha-nvim): Update dashboard

--- .config/nvim/lua/plugins/alpha-nvim.lua
+++ .config/nvim/lua/plugins/alpha-nvim.lua
@@ -9,12 +9,12 @@ dashboard.section.header.val = {
 
 dashboard.section.buttons.val = {
     dashboard.button("i",     "󰈔  New file", ":ene <BAR> startinsert<CR>"),
+    dashboard.button(", s p", "󰃀  Load workspaces", ":Telescope workspaces<CR>"),
+    dashboard.button(", m l", "󰁯  Load session", "<cmd>SessionManager load_session<CR>"),
     dashboard.button(", s f", "  Search file", ":lua require('telescope.builtin').find_files({hidden=true, no_ignore=true})<CR>"),
     dashboard.button(", ?",   "󰈢  Find recently opened files", ":lua require('telescope.builtin').oldfiles()<CR>"),
     dashboard.button(", s g", "󰈬  Search by Grep", ":lua require('telescope.builtin').live_grep()<CR>"),
     dashboard.button(", f m", "󰪶  File manager", ":NvimTreeToggle<CR>"),
-    dashboard.button("m",     "󰃀  Jump to bookmarks", ":Marks<CR>"),
-    dashboard.button(", m l", "󰁯  Load session", "<cmd>SessionManager load_session<CR>"),
     dashboard.button("q",     "󰅚  Quit NVIM", ":qa<CR>"),
 }
 
