d2f9d4d0ede3faf8446668d14a0304df8925c509
Vadym-Valdis Yudaiev <judaew@outlook.de>
2023-07-30 00:49:42 +0300

    nvim(workspaces): Remove lazy for plugin due to requested for start page

--- .config/nvim/lua/lazy/ide/workspaces.lua
+++ .config/nvim/lua/lazy/ide/workspaces.lua
@@ -1,13 +1,7 @@
 return {
     {
         "natecraddock/workspaces.nvim",
-        cmd = {
-            "WorkspacesAdd", "WorkspacesList", "WorkspacesOpen",
-            "WorkspacesAddDir", "WorkspacesRemove", "WorkspacesRename",
-            "WorkspacesListDirs", "WorkspacesSynsDirs", "WorkspacesRemoveDir"
-        },
-        keys = { "<Leader>sp", "[S]earch [P]rojects"},
-        dependencies = { "nvim-telescope/telescope.nvim" },
+        lazy = false,
         config = function()
             require('plugins.telescope').workspaces()
             require('plugins.telescope').workspaces_keys()
