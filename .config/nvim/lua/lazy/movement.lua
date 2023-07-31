f3e0cb892e4bc28ac8996186400ddbcee79fe26a
Vadym-Valdis Yudaiev <judaew@outlook.de>
2023-08-01 02:59:58 +0300

    nvim: Switch from vista.vim to symbols-outline.nvim

--- .config/nvim/lua/lazy/movement.lua
+++ .config/nvim/lua/lazy/movement.lua
@@ -31,14 +31,47 @@ return {
         end
     },
 
-    -- Viewer & Finder for LSP symbols and tags
+    -- Viewer & Finder for LSP symbols
     {
-        -- TODO:
-        -- * Show tag in the statusline, see:
-        --   https://github.com/liuchengxu/vista.vim#show-the-nearest-methodfunction-in-the-statusline
-        "liuchengxu/vista.vim",
-        lazy = false,
-        config = function() require("plugins.tagbar").keys() end
+        "simrat39/symbols-outline.nvim",
+        keys = {
+            { "<F9>", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" }
+        },
+        cmd = "SymbolsOutline",
+        opts = {
+            relative_width = false,
+            width = 40,
+            symbols = {
+                File          = { icon = "󰈙 ", hl = "@text.uri" },
+                Module        = { icon = " ", hl = "@namespace" },
+                Namespace     = { icon = "󰅪 ", hl = "@namespace" },
+                Package       = { icon = "󰏗 ", hl = "@namespace" },
+                Class         = { icon = "󰠱 ", hl = "@type" },
+                Method        = { icon = "󰆧 ", hl = "@method" },
+                Property      = { icon = "󰜢 ", hl = "@method" },
+                Field         = { icon = "󰇽 ", hl = "@field" },
+                Constructor   = { icon = " ", hl = "@constructor" },
+                Enum          = { icon = " ", hl = "@type" },
+                Interface     = { icon = " ", hl = "@type" },
+                Function      = { icon = "󰊕 ", hl = "@function" },
+                Variable      = { icon = "󰂡 ", hl = "@constant" },
+                Constant      = { icon = "󰏿 ", hl = "@constant" },
+                String        = { icon = "󱌯 ", hl = "@string" },
+                Number        = { icon = "󰎠 ", hl = "@number" },
+                Boolean       = { icon = "󰦍 ", hl = "@boolean" },
+                Array         = { icon = "󱒅 ", hl = "@constant" },
+                Object        = { icon = "󰀚 ", hl = "@type" },
+                Key           = { icon = "󰌋 ", hl = "@type" },
+                Null          = { icon = "󰟢 ", hl = "@type" },
+                EnumMember    = { icon = " ", hl = "@field" },
+                Struct        = { icon = " ", hl = "@type" },
+                Event         = { icon = " ", hl = "@type" },
+                Operator      = { icon = "󰆕 ", hl = "@operator" },
+                TypeParameter = { icon = "󰅲 ", hl = "@parameter" },
+                Component     = { icon = " ", hl = "@function" },
+                Fragment      = { icon = " ", hl = "@constant" },
+            }
+        }
     },
 
     -- UndoTree
