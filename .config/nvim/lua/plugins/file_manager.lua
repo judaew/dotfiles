d16697d89b8185bbc1c46da5d446a8f3b3b0aaac
Vadym-Valdis Yudaiev <judaew@outlook.de>
2023-08-14 23:42:53 +0300

    nvim: Enable netrw for open links via `gx`

--- .config/nvim/lua/plugins/file_manager.lua
+++ .config/nvim/lua/plugins/file_manager.lua
@@ -1,10 +1,6 @@
 local M = {}
 
 function M.config()
-    -- disable netrw at the very start of your init.lua
-    vim.g.loaded_netrw = 1
-    vim.g.loaded_netrwPlugin = 1
-
     require("nvim-tree").setup({
         hijack_cursor = true,
         view = {
