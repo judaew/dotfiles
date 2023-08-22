e5d2423dac789c3da33e30d79f605c8d9d8bb826
Vadym-Valdis Yudaiev <judaew@outlook.de>
2023-08-22 03:42:35 +0300

    nvim(theme): Convert VimL to Lua

--- .config/nvim/lua/lazy/themes.lua
+++ .config/nvim/lua/lazy/themes.lua
@@ -3,11 +3,11 @@ return {
         "fatih/molokai",
         enabled = true,
         priority = 1000,
-        config = function() vim.cmd ([[
-            set background=dark
-            colorscheme molokai
-            let g:molokai_origin=1
-            let g:rehash=1
-        ]]) end
+        config = function()
+            vim.opt.background="dark"
+            vim.cmd.colorscheme("molokai")
+            vim.g.molokai_origin=1
+            vim.g.rehash=1
+    end
     },
 }
