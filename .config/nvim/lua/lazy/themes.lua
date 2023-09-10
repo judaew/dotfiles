91d453b5c8de4fe815379fb489529106d92e1385
Vadym-Valdis Yudaiev <judaew@outlook.de>
2023-09-10 22:55:30 +0300

    nvim: Switch to ronny.nvim colorscheme
    See https://github.com/judaew/ronny.nvim

--- .config/nvim/lua/lazy/themes.lua
+++ .config/nvim/lua/lazy/themes.lua
@@ -1,13 +1,10 @@
 return {
     {
-        "fatih/molokai",
-        enabled = true,
+        dir = "~/Workspaces/github.com/judaew/ronny.nvim",
         priority = 1000,
         config = function()
-            vim.opt.background="dark"
-            vim.cmd.colorscheme("molokai")
-            vim.g.molokai_origin=1
-            vim.g.rehash=1
-    end
+            vim.cmd.colorscheme("ronny")
+            require("ronny").setup({})
+        end,
     },
 }
