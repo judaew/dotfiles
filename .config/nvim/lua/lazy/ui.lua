ec1940644e4373fa75b1f3850d0a90cde228f15c
Vadym-Valdis Yudaiev <judaew@outlook.de>
2024-01-24 17:17:32 +0200

    nvim(todo-comments,colorizer,indent-blankline): Merge conf to ../lazy/*.lua

--- .config/nvim/lua/lazy/ui.lua
+++ .config/nvim/lua/lazy/ui.lua
@@ -20,21 +20,89 @@ return {
         dependencies = {
             "nvim-lua/plenary.nvim"
         },
-        config = function() require("plugins.todo-comments").config() end
+        config = function()
+            require("todo-comments").setup({
+                signs = true,
+                sign_priority = 8,
+                keywords = {
+                    TODO = { icon = " ", color = "info" },
+                    NOTE = { icon = " ", color = "hint", alt = { "INFO" }},
+                    FIX  = { icon = " ", color = "error", alt =
+                        { "FIXME", "BUG", "FIXIT", "ISSUE" }},
+                    HACK = { icon = " ", color = "warning" },
+                    WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" }},
+                    PERF = { icon = " ", color = "perf", alt =
+                        { "OPTIM", "PERFORMANCE", "OPTIMIZE" }},
+                    TEST = { icon = " ", color = "test", alt =
+                        { "TESTING", "PASSED", "FAILED" }},
+                },
+                colors = {
+                    info = { "#66D9EF" },
+                    hint = { "#CCCCCC" },
+                    error = { "#F92672" },
+                    warning = { "#FD971F" },
+                    perf = { "#A6E22E" },
+                    default = { "Identifier", "#AE81FF" },
+                    test = { "#7E8E91" }
+                },
+                merge_keywords = true,
+                highlight = {
+                    multiline = false,
+                    before = "",
+                    keyword = "bg",
+                    after = "",
+                    comments_only = true
+                },
+            })
+        end
     },
 
     -- The fastest colorizer, #a6e22e
     {
         "NvChad/nvim-colorizer.lua",
         lazy = false,
-        config = function() require("plugins.nvim-colorizer").config() end
+        config = function()
+            require("colorizer").setup({
+                filetypes = { "*"},
+                user_default_options = {
+                    RGB = false,
+                    names = false
+                }
+            })
+        end
     },
 
     -- Indent guides for Neovim
     {
         "lukas-reineke/indent-blankline.nvim",
         lazy = false,
-        config = function() require("plugins.indent-blankline").config() end
+        config = function()
+            require("ibl").setup({
+                indent = {
+                    char = "▏" -- or "│"
+                },
+                scope = {
+                    show_start = false,
+                    show_end = false,
+                    injected_languages = true
+                },
+                exclude = {
+                    filetypes = {
+                        "TelescopePrompt", "TelescopeResults",
+                        "alpha",
+                        "help",
+                        "lazy",
+                        "lspinfo",
+                        "terminal",
+                        "org",
+                        ""
+                    },
+                    buftypes = {
+                        "terminal", "nofile", "quickfix", "prompt"
+                    }
+                }
+            })
+        end
     },
 
     -- Rename tabs
