79edca5222f495404b4a533e92d479b1ba34b690
Vadym-Valdis Yudaiev <judaew@outlook.de>
2024-01-28 13:10:32 +0200

    nvim(overseer,ghq): Merge conf to ../lazy/*.lua

--- .config/nvim/lua/lazy/ide/workspaces.lua
+++ .config/nvim/lua/lazy/ide/workspaces.lua
@@ -1,9 +1,14 @@
+local map = function(keys, func, desc)
+    vim.keymap.set("n", keys, func, { desc=desc, noremap=true})
+end
+
 return {
     {
         "nvim-telescope/telescope-ghq.nvim",
         config = function()
-            require('plugins.telescope').workspaces()
-            require('plugins.telescope').workspaces_keys()
+            pcall(require("telescope").load_extension, "ghq")
+
+            map("<Leader>sp", ":Telescope ghq list<CR>", "[S]earch [P]roject")
         end
     }
 }
