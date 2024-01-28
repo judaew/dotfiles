e1c0c701dfab3d21127407959d608427d2120bb0
Vadym-Valdis Yudaiev <judaew@outlook.de>
2024-01-28 13:09:26 +0200

    nvim(sniprun,dap{,ui},vsnip,session_manager): Merge conf to ../lazy/.*lua

--- .config/nvim/lua/lazy/ide/dap.lua
+++ .config/nvim/lua/lazy/ide/dap.lua
@@ -26,12 +26,101 @@ return {
             -- plugin for UI
             {
                 "rcarriga/nvim-dap-ui",
-                config = function() require("plugins.ide.dap").dapui() end,
+                config = function()
+                    local dapui = require("dapui")
+
+                    dapui.setup({
+                        layouts = {
+                            {
+                                elements = {
+                                    { id = "scopes",      size = 0.25 },
+                                    { id = "breakpoints", size = 0.20 },
+                                    { id = "stacks",      size = 0.25 },
+                                    { id = "watches",     size = 0.30 },
+                                },
+                                size = 50,
+                                position = "right",
+                            },
+                            {
+                                elements = { "repl" },
+                                size = 10,
+                                position = "bottom",
+                            },
+                        },
+                        windows = { indent = 1 },
+                    })
+                end,
             }
         },
         config = function()
+            local dap   = require("dap")
+            local dapui = require("dapui")
+
+            -- See https://github.com/mfussenegger/nvim-dap/wiki/Debug-Adapter-installation#ccrust-via-lldb-vscode
+            dap.adapters.lldb = {
+                type = "executable",
+                command = vim.fn.exepath("lldb-vscode-mp-16"),
+                name = "lldb",
+                env = {
+                    LLDB_LAUNCH_FLAG_LAUNCH_IN_TTY = "YES"
+                },
+            }
+
+            dap.configurations.cpp = {
+                {
+                    name = "Launch",
+                    type = "lldb",
+                    request = "launch",
+                    program = function()
+                        return vim.fn.input("Path to executable: " .. vim.fn.getcwd() .. "/")
+                    end,
+                    cwd = "${workspaceFolder}",
+                    stopOnEntry = false,
+                    runInTerminal = false,
+                    externalTerminal = false,
+                    args = {}
+                }
+            }
+
+            dap.configurations.c = dap.configurations.cpp
+            dap.configurations.rust = dap.configurations.cpp
+
+            dap.listeners.after.event_initialized["dapui_config"] = function()
+                dapui.open()
+            end
+            dap.listeners.before.event_terminated["dapui_config"] = function()
+                dapui.close()
+            end
+            dap.listeners.before.event_exited["dapui_config"] = function()
+                dapui.close()
+            end
             require("plugins.ide.dap").dap()
-            require("plugins.ide.dap").keys()
+
+            -- Keymaps
+            -- See https://stackoverflow.com/a/52368238
+            local map = function(keys, func, desc)
+                if desc then
+                    desc = "DAP: " .. desc
+                end
+                vim.keymap.set("n", keys, func, { desc=desc, noremap=true})
+            end
+
+            map("<Leader>db",  dap.toggle_breakpoint, "Creates or removes a [b]reakpoint")
+            map("<Leader>dc",  dap.continue,          "Start/[C]ontinue debugging")
+            map("<Leader>dsi", dap.step_into,         "[S]tep [i]nto a function or method")
+            map("<Leader>dso", dap.step_over,         "[S]tep [o]ver for <count> steps")
+            map("<Leader>dsq", dap.step_out,          "[S]tep out of a function or method")
+            map("<Leader>dsb", dap.step_back,         "[S]tep one step [b]ack")
+            map("<Leader>dr",  dap.run_to_cursor,     "[R]un to the current cursor")
+            map("<Leader>dq",  dap.terminate,         "Terminates the debug session")
+
+            -- Function keys
+            map("<F10>",   dap.toggle_breakpoint, "Toggle breakpoint")
+            map("<S-F10>", dap.terminate,         "Terminates the debug session")
+            map("<F11>",   dap.continue,          "Start/Continue debugging")
+            map("<S-F11>", dap.step_out,          "Step out of a function or method")
+            map("<F12>",   dap.step_into,         "Step into a function or method")
+            map("<S-F12>", dap.step_over,         "Step over for [count] steps")
         end
     }
 }
--- .config/nvim/lua/lazy/ide/session_manager.lua
+++ .config/nvim/lua/lazy/ide/session_manager.lua
@@ -1,3 +1,7 @@
+local map = function(keys, func, desc)
+    vim.keymap.set("n", keys, func, { desc=desc, noremap=true})
+end
+
 return {
     {
         "Shatur/neovim-session-manager",
@@ -6,8 +10,17 @@ return {
             "nvim-lua/plenary.nvim"
         },
         config = function()
-            require("plugins.session_manager").config()
-            require("plugins.session_manager").keys()
+            require("session_manager").setup({
+                -- Open a session in the current directory if it exist
+                autoload_mode = require("session_manager.config").AutoloadMode.CurrentDir,
+                -- Create a session manually
+                autosave_only_in_session = true
+            })
+
+            -- Keymaps
+            map("<Leader>ms", "<cmd>SessionManager save_current_session<CR>", "Session Manager: Save")
+            map("<Leader>ml", "<cmd>SessionManager load_session<CR>",         "Session Manager: Load")
+            map("<Leader>md", "<cmd>SessionManager delete_session<CR>",       "Session Manager: Delete")
         end
     }
 }
