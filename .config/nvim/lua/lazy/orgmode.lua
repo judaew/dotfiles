e011e75f990a357b568877e9e5d88eb046af00a0
Vadym-Valdis Yudaiev <judaew@outlook.de>
2024-06-26 16:26:58 +0300

    nvim(orgmode): setup_ts_grammar is no longer needed

--- .config/nvim/lua/lazy/orgmode.lua
+++ .config/nvim/lua/lazy/orgmode.lua
@@ -3,7 +3,6 @@ return {
     {
         "nvim-orgmode/orgmode",
         config = function()
-            require("orgmode").setup_ts_grammar()
             require("orgmode").setup({
                 org_agenda_files = "~/Org/**/*",
                 org_default_notes_file = "~/Org/Notes.org",
