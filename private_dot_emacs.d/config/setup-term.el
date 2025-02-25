0d8687156b2a2386ffb5c8c4651d01db5e616923
Vadym-Valdis Yudaiev <judaew@outlook.de>
2025-02-25 18:21:00 +0200

    emacs: add exec-path-from-shell and mouse pkg

--- private_dot_emacs.d/config/setup-term.el
+++ private_dot_emacs.d/config/setup-term.el
@@ -4,6 +4,12 @@
 
 ;;; Code:
 
+(use-package exec-path-from-shell
+  :config
+  (setq exec-path-from-shell-arguments (list "-l"))
+  (when (memq window-system '(mac ns x pgtk))
+    (exec-path-from-shell-initialize)))
+
 (use-package vterm
   :bind ("C-c t" . vterm)
   :custom
@@ -11,5 +17,10 @@
 
 (use-package direnv
   :hook (after-init . direnv-mode))
+
+(use-package mouse
+  :straight nil
+  :unless (display-graphic-p)
+  :hook (after-init . xterm-mouse-mode))
 
 ;;; setup-term.el ends here
