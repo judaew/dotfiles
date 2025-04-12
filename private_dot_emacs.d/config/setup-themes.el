23fe787636c72fc924a54214d6f6048d93cf49f3
Vadym-Valdis Yudaiev <judaew@outlook.de>
2025-04-13 02:07:20 +0300

    emacs(themes): Use doom-themes

--- private_dot_emacs.d/config/setup-themes.el
+++ private_dot_emacs.d/config/setup-themes.el
@@ -1,14 +1,26 @@
-;;; Package --- Summary
+;;; Package --- Summary -*- lexical-binding: t; -*-
 
 ;;; Commentary:
 
 ;;; Code:
 
 (use-package monokai-theme
+  :disabled
+  :straight (monokai-theme :host github :repo "oneKelvinSmith/monokai-emacs"
+                           :fork (:host github :repo "bohonghuang/monokai-emacs" :branch "tab-bar"))
+  ;; :straight (monokai-theme :host github :repo "bohonghuang/monokai-emacs" :branch "tab-bar")
   :config
   (load-theme 'monokai t)
 
   ;; Use italic for comments
   (set-face-attribute 'font-lock-comment-face nil :slant 'italic))
+
+(use-package doom-themes
+  :custom
+  (doom-themes-enable-bold t)
+  (doom-themes-enable-italic t)
+  :config
+  (load-theme 'doom-molokai t)
+  (doom-themes-org-config))
 
 ;;; setup-themes.el ends here
