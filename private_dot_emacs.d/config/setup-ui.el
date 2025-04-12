38f714389ab5dd297b5ab92daf1f786d304893e9
Vadym-Valdis Yudaiev <judaew@outlook.de>
2025-04-13 02:40:30 +0300

    emacs(ui): Add goggles, indent-bars, colorful-mode and enable pixel-scroll-*-mode

--- private_dot_emacs.d/config/setup-ui.el
+++ private_dot_emacs.d/config/setup-ui.el
@@ -17,9 +17,13 @@
     (all-the-icons-install-fonts t)))
 
 (use-package doom-modeline
-  :hook (after-init . doom-modeline-mode))
+  :hook (after-init . doom-modeline-mode)
+  :config
+  (setq doom-modeline-bar-color "#2D2E2E"))
 
+;; Build-in from Emacs 30 (#emacs30)
 (use-package which-key
+  :straight nil
   :diminish which-key-mode
   :hook (after-init . which-key-mode))
 
@@ -45,5 +49,36 @@
 (use-package highlight-parentheses
   :diminish highlight-parentheses-mode
   :hook (emacs-lisp-mode . highlight-parentheses-mode))
+
+(use-package goggles
+  :hook ((prog-mode text-mode) . goggles-mode)
+  :config
+  (setq-default goggles-pulse t))
+
+(use-package indent-bars
+  :hook ((c-mode
+	  c++-mode
+	  go-mode
+	  rust-mode
+	  python-mode
+	  lua-mode
+	  yaml-mode) . indent-bars-mode)
+  :custom
+  (indent-bars-no-descend-lists t) ; no extra bars in continued func arg lists
+  (indent-bars-treesit-support t)
+  (indent-bars-treesit-ignore-blank-lines-types '("module")))
+
+(use-package colorful-mode
+  :defer t
+  :hook (css-mode
+	 html-mode
+	 dhall-mode
+	 json-mode
+	 yaml-mode))
+
+(use-package emacs
+  :straight nil
+  :if (display-graphic-p)
+  :hook (after-init . pixel-scroll-precision-mode))
 
 ;;; setup-ui.el ends here
