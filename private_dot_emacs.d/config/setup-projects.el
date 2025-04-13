15e81f7b24abd358a00830207af0397501aa18be
Vadym-Valdis Yudaiev <judaew@outlook.de>
2025-04-13 03:00:40 +0300

    emacs: Move from projectile to project (build-in)

--- private_dot_emacs.d/config/setup-projects.el
+++ private_dot_emacs.d/config/setup-projects.el
@@ -4,15 +4,22 @@
 
 ;;; Code:
 
-(use-package projectile
-  :diminish projectile-mode
-  :bind-keymap ("C-c p" . projectile-command-map)
-  :hook (after-init . projectile-mode)
-  :custom
-  (projectile-switch-project-action #'projectile-dired)
-  (projectile-project-search-path '(
-                                    "~/Workspaces/local"
-                                    "~/Workspaces/github.com/judaew"))
-  (projectile-completion-system 'ivy))
+;; require emacs 27.1
+(use-package project
+  :straight nil
+  :config
+  (setq project-switch-use-consult t)
+
+  (setq project-switch-commands
+	'((project-find-file "Find file" "f")
+	  (project-find-regexp "Find regexp" "r")
+	  (project-dired "Dired" "d")
+	  (magit-project-status "Magit" "m"))))
+
+(use-package consult-project-extra
+  :disabled
+  :bind
+  (("C-c p f" . consult-project-extra-find)
+   ("C-c p o" . consult-project-extra-find-other-window)))
 
 ;;; setup-projects.el ends here
