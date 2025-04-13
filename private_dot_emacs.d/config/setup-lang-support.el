98151e28b134524b2236322df9a67012f1b7b523
Vadym-Valdis Yudaiev <judaew@outlook.de>
2025-04-13 03:06:48 +0300

    emacs: Setup podman and add switch podman/docker

--- private_dot_emacs.d/config/setup-lang-support.el
+++ private_dot_emacs.d/config/setup-lang-support.el
@@ -6,6 +6,38 @@
 
 ;;; Code:
 
+(defun set-flyspell-language-based-on-content ()
+  (interactive)
+  (let ((text (thing-at-point 'line t)))
+    (cond
+     ((string-match-p "\\b[А-Яа-я]" text)
+      (setq ispell-dictionary "russian"))
+     ((string-match-p "\\b[a-zA-Z]\b" text)
+      (setq ispell-dictionary "english"))
+     (t
+      (setq ispell-dictionary "english")))))
+
+(use-package flyspell
+  :hook
+  ((text-mode . flyspell-mode)
+   (prog-mode . flyspell-prog-mode))
+  :init
+  ;; (add-hook 'flyspell-mode-hook
+  ;;           (lambda ()
+  ;;             (add-hook 'post-command-hook 'set-flyspell-language-based-on-content nil t)))
+  :config
+  (setq ispell-program-name "hunspell")
+  (setq ispell-dictionary "english")
+  (setq ispell-dictionary-alist
+        '(("english" "en_US" "[[:alpha:]]" "[^[:alpha:]]" nil ("-d" "en_US"))
+          ("ukrainian" "uk_UA" "[[:alpha:]]" "[^[:alpha:]]" nil ("-d" "uk_U"))
+          ("russian" "ru_RU" "[[:alpha:]]" "[^[:alpha:]]" nil ("-d" "ru_RU")))))
+
+(use-package ispell
+  :ensure t
+  :config
+  (global-set-key (kbd "C-c i") 'ispell-change-dictionary))
+
 ;; Syntax highlighting for systemd files
 (add-to-list 'auto-mode-alist '("\\.service\\'" . conf-unix-mode))
 (add-to-list 'auto-mode-alist '("\\.timer\\'" . conf-unix-mode))
@@ -33,11 +65,36 @@
   :straight nil
   :defer)
 
-;; TODO: set :bind ("C-c d" . docker)
+(defcustom my/emacs-docker-executable 'podman
+  "The executable to be used with docker-mode."
+  :type '(choice
+	  (const :tag "docker" docker)
+	  (const :tag "podman" podman))
+  :group 'my/emacs)
+
 (use-package docker
-  :defer t)
+  :defer t
+  :bind ("C-c d" . docker)
+  :config
+  (pcase my/emacs-docker-executable
+    ('docker
+     (setf docker-command "docker"
+	   docker-compose-command "docker-compose"
+	   docker-container-tramp-method "docker"))
+    ('podman
+     (setf docker-command "podman"
+	   docker-compose-command "podman-compose"
+	   docker-container-tramp-method "podman"))))
+
 (use-package dockerfile-mode
-  :defer t)
+  :defer t
+  :config
+  (pcase my/emacs-docker-executable
+    ('docker
+     (setq dockerfile-mode-command "docker"))
+    ('podman
+     (setq dockerfile-mode-command "podman"))))
+
 (use-package docker-compose-mode
   :defer t)
 
