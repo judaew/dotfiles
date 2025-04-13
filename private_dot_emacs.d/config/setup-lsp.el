5f68d175e7535f8d05f0f5308863594ec44c7380
Vadym-Valdis Yudaiev <judaew@outlook.de>
2025-04-13 03:04:14 +0300

    emacs(lsp): Enable/disable few opts, conf corfu, kill LSP if all buffers killed

--- private_dot_emacs.d/config/setup-lsp.el
+++ private_dot_emacs.d/config/setup-lsp.el
@@ -16,21 +16,34 @@
      dhall-mode
      dockerfile-mode) . lsp-deferred)
    (lsp-mode . lsp-enable-which-key-integration)
+   ;; See https://magnus.therning.org/2024-05-04-orderless-completion-in-lsp-mode.html
+   (lsp-completion-mode . (lambda ()
+				 (setq-local completion-category-defaults
+					     (assoc-delete-all 'lsp-capf completion-category-defaults)))))
   :custom
-  (lsp-keymap-prefix "C-l")
-  (lsp-auto-guess-root t)
+  (lsp-keymap-prefix "C-c l")
   (lsp-use-plist t)
   (lsp-signature-doc-lines 10)
-  :commands (lsp lsp-deferred))
+  (lsp-inlay-hint-enable t)
+  (lsp-auto-guess-root t)
+
+  ;; Use corfu and cape as autocomplete
+  (lsp-enable-completion-at-point t)
+  (lsp-completion-provider :none)
+
+  ;; Kill LSP servers if project closed
+  ;; Eg `projectile-kill-buffers' (C-x p k)
+  (lsp-keep-workspace-alive nil)
+  :commands (lsp lsp-deferred)
+  :config
+  ;; See https://github.com/minad/corfu/issues/188
+  (advice-add #'lsp-completion-at-point :around #'cape-wrap-noninterruptible))
 
 (use-package lsp-ui
   :after lsp-mode)
 
-(use-package lsp-ivy
-  :after lsp-mode)
-
 (use-package lsp-pyright
-  :custom (lsp-pyright-langserver-command "pyright") ;; or basedpyright
+  :custom (lsp-pyright-langserver-command "pyright")
   :hook (python-mode . (lambda ()
                          (require 'lsp-pyright)
                          (lsp-deferred))))
