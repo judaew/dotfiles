78c432b120894fb3bf224829a345c1316d4ed242
Vadym-Valdis Yudaiev <judaew@outlook.de>
2025-10-23 04:08:47 +0300

    emacs(themes): Switch from monokai-theme to doom-themes (using doom-molokai)

--- private_dot_emacs.d/config/setup-themes.el
+++ private_dot_emacs.d/config/setup-themes.el
@@ -1,19 +1,11 @@
-;;; Package --- Summary -*- lexical-binding: t; -*-
+;;; setup-themes.el --- Themes and appearance -*- lexical-binding: t; -*-
 
 ;;; Commentary:
 
+;; Packages:
+;; - `doom-themes' ~ collection of Doom Emacs themes
+
 ;;; Code:
-
-(use-package monokai-theme
-  :disabled
-  :straight (monokai-theme :host github :repo "oneKelvinSmith/monokai-emacs"
-                           :fork (:host github :repo "bohonghuang/monokai-emacs" :branch "tab-bar"))
-  ;; :straight (monokai-theme :host github :repo "bohonghuang/monokai-emacs" :branch "tab-bar")
-  :config
-  (load-theme 'monokai t)
-
-  ;; Use italic for comments
-  (set-face-attribute 'font-lock-comment-face nil :slant 'italic))
 
 (use-package doom-themes
   :custom
@@ -21,6 +13,10 @@
   (doom-themes-enable-italic t)
   :config
   (load-theme 'doom-molokai t)
-  (doom-themes-org-config))
+  (doom-themes-org-config)
 
+  ;; Use italic for comments
+  (set-face-attribute 'font-lock-comment-face nil :slant 'italic))
+
+(provide 'setup-themes)
 ;;; setup-themes.el ends here
