d9d0e5f61385eb7e21306b59ad1cbd14ba668d81
Vadym-Valdis Yudaiev <judaew@outlook.de>
2025-10-29 00:12:24 +0200

    emacs(project): migration to ottp from project-tab-groups

--- private_dot_emacs.d/config/setup-project.el
+++ private_dot_emacs.d/config/setup-project.el
@@ -5,13 +5,12 @@
 ;; Packages:
 ;; - `project'               ~ project management core
 ;; - `consult-project-extra' ~ enhanced project navigation
-;; - `project-tab-groups'    ~ organize projects in tab groups
+;; - `ottp'                  ~ organize projects in tab groups
 
 ;;; Code:
 
 (use-package project
   :config
-
   (add-hook 'project-find-functions #'project-try-vc)
   (setq project-vc-extra-root-markers
 	'(".project"         ;; project.el (default)
@@ -36,9 +35,11 @@
   (("C-x p f" . consult-project-extra-find)
    ("C-x p o" . consult-project-extra-find-other-window)))
 
-(use-package project-tab-groups
+(use-package otpp ; one-tab-per-project
+  :after project
   :config
-  (project-tab-groups-mode 1))
+  (otpp-mode 1)
+  (otpp-override-mode 1))
 
 (provide 'setup-project)
 ;;; setup-project.el ends here
