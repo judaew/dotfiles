4b8482ec578a0e6b5992c55bdfc0398766237468
Vadym-Valdis Yudaiev <judaew@outlook.de>
2025-10-29 00:05:20 +0200

    emacs: migration to flymake [builtin]

---
+++ private_dot_emacs.d/config/setup-flymake.el
@@ -0,0 +1,19 @@
+;;; setup-flymake.el --- Syntax checking -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;; Packages:
+;; - `flymake' ~ on-the-fly syntax checking
+
+;;; Code:
+
+(use-package emacs
+  :defer 1
+  :straight nil
+  :hook (prog-mode . flymake-mode)
+  :config
+  (setopt elisp-flymake-byte-compile-load-path load-path)
+  (setq trusted-content '("~/.emacs.d/early-init.el" "~/.emacs.d/config/")))
+
+(provide 'setup-flymake)
+;;; setup-flymake.el ends here
