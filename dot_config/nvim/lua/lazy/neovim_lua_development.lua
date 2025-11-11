d5417ff5551d7fe4a85cc965e052f01e395aa863
Vadym-Valdis Yudaiev <judaew@outlook.de>
2025-11-11 14:58:19 +0200

    nvim: Delete all conf

--- dot_config/nvim/lua/lazy/neovim_lua_development.lua
+++
@@ -1,9 +0,0 @@
-return {
-  -- Plugin for automatically configures `lua-language-server` for
-  -- Neovim config, Neovim runtime and plugin directories
-  {
-    "folke/neodev.nvim",
-    ft = "lua",
-    opts = {}
-  }
-}
--- dot_config/nvim/lua/lazy/utility.lua
+++
@@ -1,6 +0,0 @@
-return {
-  {
-    "tpope/vim-eunuch",
-    lazy = false
-  }
-}
