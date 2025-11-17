de1c749ef98f67e329159b05f1ead98c74e283bd
Vadym-Valdis Yudaiev <judaew@outlook.de>
2025-11-17 02:31:10 +0200

    emacs(31*-dev): Remove obsolete conf; details in body
    There is no need to manually set treesit-language-source-alist, since `-ts-mode` in Emacs 31* will determine them independently and more accurately. They will indicate the exact version of the parser commit with which they are compatible. This is important because, for example, in go-ts-mode, after updating the parser, indentation for some elements in Emacs broke. As far as I know, Nvim does the same thing, fixing a specific version.
    
    See https://github.com/emacs-mirror/emacs/blob/80a17f7a3076a18925ca4d16189e05a80db6c646/lisp/progmodes/go-ts-mode.el

--- private_dot_emacs.d/config/setup-treesitter.el
+++ private_dot_emacs.d/config/setup-treesitter.el
@@ -3,38 +3,8 @@
 ;;; Commentary:
 
 ;; Packages:
-;; When updating, the correct parser hint can be found in the `treesit-auto' package
 
 ;;; Code:
-
-(setq treesit-language-source-alist
-      '((bash "https://github.com/tree-sitter/tree-sitter-bash")
-	(c "https://github.com/tree-sitter/tree-sitter-c")
-	(cpp "https://github.com/tree-sitter/tree-sitter-cpp")
-	(cmake "https://github.com/uyha/tree-sitter-cmake")
-	(c-sharp "https://github.com/tree-sitter/tree-sitter-c-sharp")
-	(css "https://github.com/tree-sitter/tree-sitter-css")
-	(dockerfile "https://github.com/camdencheek/tree-sitter-dockerfile")
-	(elixir "https://github.com/elixir-lang/tree-sitter-elixir")
-	(gomod "https://github.com/camdencheek/tree-sitter-go-mod")
-	(go "https://github.com/tree-sitter/tree-sitter-go")
-	(heex "https://github.com/phoenixframework/tree-sitter-heex")
-	(html "https://github.com/tree-sitter/tree-sitter-html")
-	(java "https://github.com/tree-sitter/tree-sitter-java")
-	(javascript "https://github.com/tree-sitter/tree-sitter-javascript" "master" "src")
-	(json "https://github.com/tree-sitter/tree-sitter-json")
-	(lua "https://github.com/tree-sitter-grammars/tree-sitter-lua")
-	(php "https://github.com/tree-sitter/tree-sitter-php" "master" "php/src")
-	(python "https://github.com/tree-sitter/tree-sitter-python")
-	(ruby "https://github.com/tree-sitter/tree-sitter-ruby")
-	(rust "https://github.com/tree-sitter/tree-sitter-rust")
-	(toml "https://github.com/tree-sitter/tree-sitter-toml")
-	(tsx "https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src")
-	(typescript "https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src")
-	(yaml "https://github.com/tree-sitter-grammars/tree-sitter-yaml")))
-
-;; Exec in C-:
-;; (mapc #'treesit-install-language-grammar (mapcar #'car treesit-language-source-alist))
 
 (setq major-mode-remap-alist
       '((sh-mode . bash-ts-mode)
@@ -48,6 +18,8 @@
 	(js-mode . js-ts-mode)
 	(json-mode . json-ts-mode)
 	(python-mode . python-ts-mode)
+	(lua-mode . lua-ts-mode)
+	(markdown-mode . markdown-ts-mode)
 	(ruby-mode . ruby-ts-mode)
 	(conf-toml-mode . toml-ts-mode)
 	(yaml-mode . yaml-ts-mode)))
@@ -62,7 +34,6 @@
 ;; (add-to-list 'auto-mode-alist '("\\.go\\'" . go-ts-mode))
 ;; (add-to-list 'auto-mode-alist '("go.mod\\'" . go-mod-ts-mode))
 (add-to-list 'auto-mode-alist '("\\.heex\\'" . heex-ts-mode))
-(add-to-list 'auto-mode-alist '("\\.lua\\'" . lua-ts-mode))
 ;; `php-ts-mode' don't work correct
 (add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-ts-mode))
 (add-to-list 'auto-mode-alist '("\\.tsx\\'" . tsx-ts-mode))
