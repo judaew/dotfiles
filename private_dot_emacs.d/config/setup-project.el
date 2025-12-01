69311b726387e7632e7a0deac6c9098f32e5fef8
Vadym-Valdis Yudaiev <judaew@outlook.de>
2025-12-01 03:57:07 +0200

    emacs(project): add projection and compile-multi for project.el

--- private_dot_emacs.d/config/setup-project.el
+++ private_dot_emacs.d/config/setup-project.el
@@ -3,8 +3,15 @@
 ;;; Commentary:
 
 ;; Packages:
-;; - `project'               ~ project management core
-;; - `consult-project-extra' ~ enhanced project navigation
+;; - `project'                  ~ project management core
+;; - `consult-project-extra'    ~ enhanced project navigation
+;; - `projection'               ~ project management library
+;; - `projection-multi'         ~ compile-multi extension for projection
+;; - `projection-multi-embark'  ~ embark extension for projection
+;; - `compile-multi'            ~ multi target interface to compile
+;; - `consult-compile-multi'    ~ consult extension for compile-multi
+;; - `compile-multi-nerd-icons' ~ nerd-icons extension for compile-multi
+;; - `compile-multi-embark'     ~ embark extension for compile-multi
 
 ;;; Code:
 
@@ -34,6 +41,52 @@
   :bind
   (("C-x p f" . consult-project-extra-find)
    ("C-x p o" . consult-project-extra-find-other-window)))
+
+(use-package projection
+  :hook ((after-init . global-projection-hook-mode)
+         (compilation-mode . projection-customize-compilation-mode))
+  :custom
+  (compilation-buffer-name-function
+   #'projection-customize-compilation-buffer-name-function)
+  :config
+  (with-eval-after-load 'project
+    (require 'projection))
+  :bind-keymap
+  ("C-x P" . projection-map))
+
+(use-package projection-multi
+  :bind ( :map project-prefix-map
+          ("RET" . projection-multi-compile)))
+
+(use-package projection-multi-embark
+  :after embark
+  :after projection-multi
+  :demand t
+  :config (projection-multi-embark-setup-command-map))
+
+(use-package compile-multi
+  :bind ([remap compile-multi] . projection-multi-compile)
+  :config
+  (push `(emacs-lisp-mode
+          ("emacs:checkdoc" . ,#'checkdoc))
+        compile-multi-config))
+
+(use-package consult-compile-multi
+  :after compile-multi
+  :demand t
+  :config (consult-compile-multi-mode))
+
+(use-package compile-multi-nerd-icons
+  :after nerd-icons-completion
+  :after compile-multi
+  :demand t)
+
+(use-package compile-multi-embark
+  :after embark
+  :after compile-multi
+  :demand t
+  :config (compile-multi-embark-mode +1))
+
 
 (provide 'setup-project)
 ;;; setup-project.el ends here
