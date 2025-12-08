58ba36f8376318a65536e1815279d0295d0ff481
Vadym-Valdis Yudaiev <judaew@outlook.de>
2025-12-08 13:14:32 +0200

    emacs(custom): Add custom.el

---
+++ private_dot_emacs.d/custom.el
@@ -0,0 +1,14 @@
+;;; -*- lexical-binding: t -*-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values '((elisp-flymake-byte-compile . t)))
+ '(trusted-content '("~/.emacs.d/config/")))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(gnus-group-news-low ((t (:inherit nil :foreground "#555556")))))
