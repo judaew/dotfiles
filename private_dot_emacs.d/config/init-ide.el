92585ef8930f97cb75d629cb37edb3cffd59f4f5
Vadym-Valdis Yudaiev <judaew@outlook.de>
2026-04-04 17:19:43 +0300

    emacs(tree-sitter): Use new vars instead add-to-list ad major-mode-remap-alist

--- private_dot_emacs.d/config/init-ide.el
+++ private_dot_emacs.d/config/init-ide.el
@@ -103,39 +103,8 @@ Or passes other checks that determine whether eglot should run."
 
 ;; === Tree-sitter ===
 ;; -------------------
-
-(setopt major-mode-remap-alist
-        '((sh-mode . bash-ts-mode)
-          (c-mode . c-ts-mode)
-          (c++-mode . c++-ts-mode)
-          (cmake-mode . cmake-ts-mode)
-          (csharp-mode . csharp-ts-mode)
-          (css-mode . css-ts-mode)
-          (html-mode . html-ts-mode)
-          (java-mode . java-ts-mode)
-          (js-mode . js-ts-mode)
-          (json-mode . json-ts-mode)
-          (python-mode . python-ts-mode)
-          (lua-mode . lua-ts-mode)
-          (markdown-mode . markdown-ts-mode)
-          (ruby-mode . ruby-ts-mode)
-          (conf-toml-mode . toml-ts-mode)
-          (yaml-mode . yaml-ts-mode)))
-
-(add-to-list 'auto-mode-alist '("CMakeLists\\.txt\\'" . cmake-ts-mode))
-(add-to-list 'auto-mode-alist '("\\.cmake\\'" . cmake-ts-mode))
-;; See `setup-docker.el'
-;; (add-to-list 'auto-mode-alist '("Dockerfile\\'" . dockerfile-ts-mode))
-;; (add-to-list 'auto-mode-alist '("Containerfile\\'" . dockerfile-ts-mode))
-(add-to-list 'auto-mode-alist '("\\.ex\\'" . elixit-ts-mode))
-(add-to-list 'auto-mode-alist '("\\.go\\'" . go-ts-mode))
-(add-to-list 'auto-mode-alist '("go.mod\\'" . go-mod-ts-mode))
-(add-to-list 'auto-mode-alist '("go.work\\'" . go-work-ts-mode))
-(add-to-list 'auto-mode-alist '("\\.heex\\'" . heex-ts-mode))
-(add-to-list 'auto-mode-alist '("\\.php\\'" . php-ts-mode))
-(add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-ts-mode))
-(add-to-list 'auto-mode-alist '("\\.tsx\\'" . tsx-ts-mode))
-(add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-ts-mode))
+(setopt treesit-auto-install-grammar 'always)
+(setopt treesit-enabled-modes t)
 
 ;; === DAP ===
 ;; -----------
